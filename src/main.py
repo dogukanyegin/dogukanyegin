64f4dcaac62ed4637736e32512c8513fc348045b
aikidocudogukan <aikidocudogukan@gmail.com>
2024-06-02 18:50:36 +0000

    temeller 2

--- src/main.py
+++ src/main.py
@@ -16,6 +16,14 @@ def candycrush():
 @app.route("/")
 def home():
     return render_template('index.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 def main():
     app.run(port=int(os.environ.get('PORT', 80)))
 
